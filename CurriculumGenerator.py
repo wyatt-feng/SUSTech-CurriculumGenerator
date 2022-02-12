e5736db5948f1ebadece8b6506b5fec5db6ac57a
dazhi0619 <da.zhi0619@gmail.com>
2022-02-12 09:20:26 +0800

    添加README文件，简化文件层级

---
+++ CurriculumGenerator.py
@@ -0,0 +1,59 @@
+﻿#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import openpyxl, sys, Curriculum, re, datetime
+
+def __course_start_time(num):
+    time = {
+        "1" : datetime.time(8),
+        "3" : datetime.time(10, 20),
+        "5" : datetime.time(14),
+        "7" : datetime.time(16, 20),
+        "9" : datetime.time(19)
+        }
+    return time.get(num)
+def __course_end_time(num):
+    time = {
+        "2" : datetime.time(9, 50),
+        "4" : datetime.time(12, 10),
+        "6" : datetime.time(15, 50),
+        "8" : datetime.time(18, 10),
+        "10" : datetime.time(20, 50),
+        "11" : datetime.time(21, 50)
+        }
+    return time.get(num)
+
+workbook = openpyxl.load_workbook(sys.argv[1])
+sheet = workbook.active
+term_start = sys.argv[2]
+term_start_date = datetime.date(int(term_start[0:4]), int(term_start[4:6]), int(term_start[6:]))
+term_end = sys.argv[3]
+term_end_date = datetime.date(int(term_end[0:3]), int(term_end[4:6]), int(term_start[6:]))
+curriculum = Curriculum.Curriculum()
+day = 0
+for col in sheet.iter_cols(min_col=2, max_col=6, min_row=4, max_row=9):
+    for course in col:
+        if course.value:
+            val = course.value.split("\n")
+            for i in range(len(val)//4):
+                name = val[i * 4]
+                info = val[i * 4 + 3].split("][")
+                num = re.findall("\d+", sheet["A%d"%course.row].value)
+                if info[0][-2] == '单':
+                    type = Curriculum.CourseRepetitionType.biweekly
+                    start_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day), __course_start_time(num[0]))
+                    end_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day), __course_end_time(num[1]))
+                elif info[0][-2] == '双':
+                    type = Curriculum.CourseRepetitionType.biweekly
+                    start_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day+7), __course_start_time(num[0]))
+                    end_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day+7), __course_end_time(num[1]))
+                else:
+                    type = Curriculum.CourseRepetitionType.weekly
+                    start_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day), __course_start_time(num[0]))
+                    end_time = datetime.datetime.combine(term_start_date + datetime.timedelta(days=day), __course_end_time(num[1]))
+                location = "南方科技大学-" + info[1]
+                term_start_date = datetime.datetime.combine(term_start_date, datetime.time())
+                term_end_date = datetime.datetime.combine(term_end_date, datetime.time.max)
+                Curriculum.add_course(curriculum, name, start_time, end_time, location, type, term_end_date)
+    day += 1
+
+curriculum.save_as_ics_file()
---
+++ README.md
@@ -0,0 +1,21 @@
+# SUSTech iCal课表生成工具
+
+这个脚本可以帮你把从[南科大教务系统](https://tis.sustech.edu.cn)下载的xlsx格式的课表转化为可以导入日历软件通用的iCal（.ics）格式的课表。
+
+## 为什么要生成iCal格式课表
+可以导入设备自带日历软件，从而与系统深度整合，例如基本上不占用系统资源实现提醒功能，或通过Siri等语音助手管理课程安排。
+![整合Siri](images/Siri-Integration.png)
+
+## 使用方式
+1. 首先在教务系统主界面左侧点击课表，下载xlsx格式课表。
+2. 若电脑存在[Python](https://www.python.org)环境及[openpyxl](https://openpyxl.readthedocs.io/en/stable/)，则下载[CurriculumGenerator](https://github.com/dazhi0619/CurriculumGenerator/tree/master/CurriculumGenerator)目录下的两个.py文件。
+   如果您不知道我在说什么，请[在此处](https://github.com/dazhi0619/CurriculumGenerator/releases/)下载打包好的可执行文件。
+3. 运行
+   ```
+   python3 CurriculumGenerator.py <xlsx课表文件名> <学期开始日期> <学期结束日期>
+   ```
+   若您下载的是可执行文件，请在下载文件夹中按住shift右键单击文件夹空白处，选择“在此处打开powershell窗口”，在弹出的窗口中输入
+   ```
+   .\CurriculumGenerator <xlsx课表文件名> <学期开始日期> <学期结束日期>
+   ```
+4. 将生成好的`课表.ics`导入日历软件。通常情况下直接打开即可。对于iPhone和iPad，请将此文件AirDrop到您的设备上，或设法通过Safari浏览器打开此文件。
